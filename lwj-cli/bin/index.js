28122911ab7cdacb6073caebc8d95c357b8b35dc
lwjmzla <214245088@qq.com>
2022-06-22 00:14:28 +0800

    1

--- lwj-cli/bin/index.js
+++ lwj-cli/bin/index.js
@@ -1,4 +1,42 @@
 #!/usr/bin/env node
 const LwjLib = require('lwj-lib')
-console.log('lwj-cli11')
-console.log(LwjLib.add(1,2))
+//console.log('lwj-cli11')
+// console.log(LwjLib.add(1,2))
+/**
+ * !demo
+ * lwj create --name test
+ * lwj --version
+ * lwj -V
+ */
+const argv = require('process').argv
+console.log(argv)
+const command = argv[2]
+
+if(!command) {
+  console.log('请输入指令')
+  return
+}
+
+if (command.startsWith('--') || command.startsWith('-') ) {
+  const globalOption = command.replace(/-/g,'')
+  if (globalOption === 'V' || globalOption === 'version') {
+    console.log('1.0.0')
+  }
+  return
+}
+
+const options = argv.slice(3)
+console.log(options)
+if (options.length) {
+  let [option, param] = options
+  option = option.replace('--','')
+  console.log(option, param)
+
+  if (LwjLib[command]) {
+    LwjLib[command]({option, param})
+  } else {
+    console.log('无效的指令')
+  }
+} else{
+  console.log('请传入正确的参数')
+}
--- lwj-lib/lib/index.js
+++ lwj-lib/lib/index.js
@@ -1,5 +1,8 @@
 module.exports= {
   add(x,y) {
     return x + y
+  },
+  create({option,param}) {
+    //console.log(option,param)
   }
 }
