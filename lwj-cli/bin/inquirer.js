8f6f2a2dd1a1f11715c3551c09dcbdf53bd59d25
lwjmzla <214245088@qq.com>
2022-08-13 00:06:10 +0800

    1

--- lwj-cli/bin/inquirer.js
+++ lwj-cli/bin/inquirer.js
@@ -2,11 +2,50 @@ const inquirer = require('inquirer'); // !可交互的命令行
 
 inquirer
   .prompt([
+    // {
+    //   type: 'input', // !password
+    //   name: 'name',
+    //   message: 'your name:',
+    //   default: 'default name',
+    //   validate: (v) => v === 'lwj', // !返回true才通过
+    //   transformer: (v) => 'name:' + v, 
+    //   //filter:  // !会改变最终结果
+    // },
+    // {
+    //   type: 'number',
+    //   name: 'num',
+    //   message: 'your number:',
+    // }
+    // {
+    //   type: 'confirm', // !二选一  true or false
+    //   name: 'choice',
+    //   message: 'your choice:',
+    //   default: false
+    // },
+    // {
+    //   type: 'list', // ! 单选,rawlist 差不多
+    //   name: 'choice',
+    //   message: 'your choice:',
+    //   default: 0, // !index，0开始
+    //   //choices: ['a', 'b']
+    //   choices: [
+    //     {value: 1,name: 'a'},
+    //     {value: 2,name: 'b'},
+    //     {value: 3,name: 'c'},
+    //   ]
+    // },
     {
-      type: 'input',
-      name: 'name',
-      message: 'your name:'
-    }
+      type: 'checkbox', // ! 多选
+      name: 'choice',
+      message: 'your choice:',
+      default: [1], // !index，0开始
+      //choices: ['a', 'b']
+      choices: [
+        {value: 1,name: 'a'},
+        {value: 2,name: 'b'},
+        {value: 3,name: 'c'},
+      ]
+    },
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
